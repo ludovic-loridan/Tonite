2e631c17cafa562422ce3ccd4e3d5d3300c27357
ludovic-loridan <ludovic.loridan@etudiant.univ-lille1.fr>
2012-09-29 18:23:48 +0200

    Channel and Program are now smarter, to make future loading of data a lot easier
    Programs have now an unique id
    Channels have a method to get the tonight's programs

--- templates/js/entities/Channel.js
+++ templates/js/entities/Channel.js
@@ -14,15 +14,36 @@
 
 var className = "Channel";
 
-var properties = ["id","name"];
+var properties = ["id","name","tonightPrograms"];
 
 var methods = {
+    getTonightPrograms : function () {
+        if (tonightPrograms.length === 0) {
+            this.loadTonightPrograms();
+        }
+
+        return tonightPrograms;
+    },
+
+    loadTonightPrograms : function() {
+
+        this.tonightPrograms = [];
+
+        // TODO : Do a real DB request to retrieve the programs.
+        var p1 = new Program("New York, section criminelle",20120928001500,20120928010000,this);
+        var p2 = new Program("The Office",20120928010000,20120928015500,this);
+        var p3 = new Program("Truc machin",20120928033000,20120928073000,this);
+
+        this.tonightPrograms = [p1,p2,p3];
+    }
 
 };
 
 var initializer = function (name) {
     this.name = name;
-    this.id = Channel.getIdForChannelNamed(name);
+    this.id = Channel.channelIdFromName(name);
+    this.tonightPrograms = [];
+    
     Channel.instancedChannels[name] = this;
 };
 
@@ -37,10 +58,10 @@ var staticMethods = {
         }
     },
 
-    // -- Channels Sorting --
+    // -- Channel ids --
     // ex : "TF1" -> 1, "Gulli" -> 18
-    getIdForChannelNamed : function(name) {
-        var potentialId = Channel.channelSorting.indexOf(name) + 1;
+    channelIdFromName : function(name) {
+        var potentialId = Channel.channelList.indexOf(name) + 1;
         if (potentialId !== 0) {
             return potentialId;
         } else {
@@ -50,15 +71,15 @@ var staticMethods = {
 
     // returns id of the new channel name
     manageUnknownChannelNamed : function(name) {
-        Channel.channelSorting.push(name);
-        return Channel.channelSorting.length;
+        Channel.channelList.push(name);
+        return Channel.channelList.length;
     }
 
 };
 
 var staticProperties = {
 
-    channelSorting : ["TF1","France 2","France 3","Canal+","France 5","M6","Arte"],
+    channelList : ["TF1","France 2","France 3","Canal+","France 5","M6","Arte"],
 
     instancedChannels : {}
 
--- templates/js/entities/Program.js
+++ templates/js/entities/Program.js
@@ -16,6 +16,16 @@ var className = "Program";
 var properties = ["id", "title", "subtitle", "description", "genre", "subgenre", "start", "stop", "channel"];
 
 var methods = {
+    // Returns an unique id for this program
+    getId : function() {
+        return this.channel.id.toString()+"_"+this.start+"_"+this.stop;
+    },
+
+    setId : function(value) {
+        throw "Changing the id of a program is forbidden";
+    },
+
+    // Channel can be a string or a channel object
     setChannel : function(value) {
         var potentialChannel = value;
 
@@ -29,8 +39,8 @@ var methods = {
 
 var initializer = function (title,start,stop,channel) {
     this.title   = title;
-    this.start   = start;
-    this.stop    = stop;
+    this.start   = parseInt(start,10);
+    this.stop    = parseInt(stop,10);
     this.channel = channel;
 };
 

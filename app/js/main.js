63bf472d84e570657b9a74908880071107e6854d
Ludovic Loridan <ludovic.loridan@gmail.com>
2012-11-13 19:29:59 +0100

    resolved : images problem and indexedDB is back

--- app/js/main.js
+++ app/js/main.js
@@ -10,29 +10,29 @@
 */
 
 function main() {
-    var date = (new Date()).toISO(8);
-    var xmlParser = new XMLParser(date);
-    var channelsList = xmlParser.parseData();
+    // var date = (new Date()).toISO(8);
+    // var xmlParser = new XMLParser(date);
+    // var channelsList = xmlParser.parseData();
 
-    // IndexedDBManager.initialize(function() {
-    //     DataLoader.getDataForTonite(function(channelsList) {
-    //         var header = new HeaderController(channelsList);
-    //         var clc = new ChannelsListController(channelsList);
-    //         var modal = new ModalPanelController();
+    IndexedDBManager.initialize(function() {
+        DataLoader.getDataForTonite(function(channelsList) {
+            var header = new HeaderController(channelsList);
+            var clc = new ChannelsListController(channelsList);
+            var modal = new ModalPanelController();
             
-    //         document.body.appendChild(header.view);
-    //         document.body.appendChild(clc.view);
-    //         clc.centerView();
-    //     });
-    // });
+            document.body.appendChild(header.view);
+            document.body.appendChild(clc.view);
+            clc.centerView();
+        });
+    });
 
-    var header = new HeaderController(channelsList);
-    var clc = new ChannelsListController(channelsList);
-    var modal = new ModalPanelController();
+    // var header = new HeaderController(channelsList);
+    // var clc = new ChannelsListController(channelsList);
+    // var modal = new ModalPanelController();
     
-    document.body.appendChild(header.view);
-    document.body.appendChild(clc.view);
-    clc.centerView();
+    // document.body.appendChild(header.view);
+    // document.body.appendChild(clc.view);
+    // clc.centerView();
 }
 
 window.addEventListener("load", main);
